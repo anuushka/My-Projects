92305c2bf90156729f7144d87973ac971906df3a
Tugsjargal <tugs.sw@gmail.com>
2018-07-04 11:05:03 +0800

    place.rb

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,13 @@ class Place < ApplicationRecord
 	has_and_belongs_to_many :categories 
 	mount_uploader :image,ImageUploader
 	belongs_to :user
+
+	
+	def self.search(search)
+		if search
+			 where(["title LIKE ?", "%#{search}%"])
+		else
+			order("updated_at DESC")
+		end
+	end 
 end
