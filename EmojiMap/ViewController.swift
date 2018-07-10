2152cf5a2d8a71542ae61fe36aca23c600e87678
Anujin Baatartsogt <Ujin@kazus-macbook-pro.local>
2018-07-10 15:05:48 +0800

    Initial Commit

---
+++ EmojiMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiMap
+//
+//  Created by Anujin Baatartsogt on 7/10/18.
+//  Copyright © 2018 Anujin Baatartsogt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
